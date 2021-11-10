f02c63760740720322774f2030acf3d5ee9a2163
Jason <LutzJason92@gmail.com>
2021-11-09 23:56:54 -0700

    html, JS and CSS edits
    edited wording within html elements. created array and linked to HTML in js. CSS styling edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,35 @@
-//DECLARE VARIABLES
+// // //DECLARE VARIABLES
+
+var quizQuestions = [
+
+    { question : "sample",
+    choices : ["a", "b", "c", "d"],
+    answer : "b" },
+
+//    { question :
+//     choices :
+//     answer :  },
+
+//     { question :
+//     choices :
+//     answer : },
+
+//     { question :
+//     choices :
+//     answer : },
+
+//     { question :
+//     choices :
+//     answer : },
+
+//     { question :
+//     choices :
+//     answer : },
+
+//     { question :
+//     choices :
+//     answer : }
+];
 
 
 
@@ -6,35 +37,55 @@ var startCountDown = document.getElementById("timer")
 
 var beginQuestion1 = document.getElementById("questions")
 
+var displayAnswers = document.getElementById("answers")
+
 var secondsLeft = 60;
 
+var questionAndanswersArray = 0;
+
+var ulChoices = document.createElement("ul")
 
 //EventListener - button clicks
 
 
 
-document.getElementById("button").addEventListener('click', startTimer)
+document.getElementById("button").addEventListener('click', startTimer);
 
-document.getElementById("button").addEventListener('click', beginQuestion1)
+
 
 function startTimer (){
     var timerInterval = setInterval(function() {
+        // beginQuestion1()
         secondsLeft--;
         startCountDown.textContent = secondsLeft + " seconds remaining.";
     
-        if(secondsLeft < 15) 
+        if(secondsLeft < 10) 
         {
             startCountDown.textContent = secondsLeft + " time is almost up!";
-        } 
+        }; 
+        
         // else (secondsLeft === 0); { 
             
         //     clearInterval(timerInterval);
-           
+        //     gameOver()
         // };
-      }, 1000); //milliseconds 
+      }, 1000); //milliseconds
+      
+    questions() 
 };
 
-function beginQuestion1() {
-        beginQuestion1
+function questions(){
+        beginQuestion1.textContent = "question Place Holder"
+        displayAnswers.textContent = "answer Place holder"
 
-}
+        for (var i = 0; i < quizQuestions.length; i++)
+            var displayQuestion = quizQuestions[questionAndanswersArray].question
+            var displayChoices = quizQuestions[questionAndanswersArray].choices;
+            beginQuestion1.textContent = displayQuestion;
+    
+};
+
+// function gameOver() {
+
+// };
+

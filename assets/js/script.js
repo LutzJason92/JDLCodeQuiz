6820b9cc35a77c5bf62a96a63f0a4d9caa6fecbf
Jason <LutzJason92@gmail.com>
2021-11-10 23:24:14 -0700

    js edits
    linked points to html page, updated questions, formatting changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 // // //DECLARE VARIABLES
 
 var startCountDown = document.getElementById("timer")
-var beginQuestion1 = document.getElementById("questions")
+var userQuestions = document.getElementById("questions")
 var displayAnswers = document.getElementById("answers")
 var displayValue = document.getElementById("rightWrong")
 var choicesArray1 = document.getElementById("choice1")
 var choicesArray2 = document.getElementById("choice2")
 var choicesArray3 = document.getElementById("choice3")
 var choicesArray4 = document.getElementById("choice4")
-
+var displayScore =  document.getElementById("points")
 
 // questionAndanswersArray
 var quizQuestions = [
 
-    { question : "sample",
-    choices : ["a", "b", "c", "d"],
-    answer : "b" },
+    { question : "The following is an example of what? var = studentNames = [ “Jason”, “Moe”, “Collin”, “Olivia”]",
+    choices : ["a string", "an array", "comma notation", "a function"],
+    answer : "an array" },
 
    { question : "sample 2",
     choices : [ 1, 2, 3, 4 ],
@@ -28,33 +28,33 @@ var quizQuestions = [
     { question : "sample 4",
     choices : [ 9, 8, 7, 6 ],
     answer : 8 }
-
-//     { question :
-//     choices :
-//     answer : },
-
-
 ];
-
 
 var quizQa = 0;
 var penalty = 5;
 var secondsLeft = 35;
-var score = 0
+var points = 0;
 
 //EventListener - button clicks
 document.getElementById("button").addEventListener('click', startTimer);
-document.getElementById("choice1").addEventListener('click', judgeMent);document.getElementById("choice2").addEventListener('click', judgeMent);document.getElementById("choice3").addEventListener('click', judgeMent);document.getElementById("choice4").addEventListener('click', judgeMent);
+document.getElementById("choice1").addEventListener('click', judgeMent);
+document.getElementById("choice2").addEventListener('click', judgeMent);
+document.getElementById("choice3").addEventListener('click', judgeMent);
+document.getElementById("choice4").addEventListener('click', judgeMent);
+
+
+
+    
 
 function startTimer (){
     var timerInterval = setInterval(function() {
-        // beginQuestion1()
+       
         secondsLeft--;
         startCountDown.textContent = secondsLeft + " seconds remaining.";
     
         if(secondsLeft < 10) 
         {
-            startCountDown.textContent = secondsLeft + " time is almost up!";
+            startCountDown.textContent = secondsLeft + " seconds remaining! time is almost up!";
         } 
         
         if (secondsLeft <= 0) { 
@@ -64,68 +64,65 @@ function startTimer (){
         }
       }, 1000); //milliseconds
       
-    questions() 
+    question1() 
 };
 
-function questions(){
-
-        //proves content is pushed to page
-        // beginQuestion1.textContent = "question Place Holder"
-        // displayAnswers.textContent = "answer Place holder"
-
+function question1(){
         for (var i = 0; i < quizQuestions.length; i++){
         
             var displayQuestion = quizQuestions[quizQa].question;
-            beginQuestion1.textContent = displayQuestion;
+            userQuestions.textContent = displayQuestion;
         };
         
-        for (var x = 0; x < quizQuestions.length; x++){ 
+        for (var i = 0; i < quizQuestions.length; i++ ){ 
             
-            var displayChoices = quizQuestions[quizQa].choices[x];
+            var displayChoices = quizQuestions[quizQa].choices[i];
             
-            if (x == 0) {
+            if (i == 0) {
                 choicesArray1.textContent = displayChoices
             }
-            if (x == 1) {
+            if (i == 1) {
                 choicesArray2.textContent = displayChoices
             }
-            if (x == 2) {
+            if (i == 2) {
                 choicesArray3.textContent = displayChoices
             }
-            if (x == 3) {
+            if (i == 3) {
                 choicesArray4.textContent = displayChoices
             } 
-                  
+                
         }
     
 }
 function judgeMent(event){
        var displayChoices = event.target;
-
-       for (var i = 0; i < quizQuestions.length; i++){
+       for (var i = 0; i < 1; i++){
         
          var displayAnswers = quizQuestions[quizQa].answer;
    
        if (displayAnswers === displayChoices.textContent){
-            displayValue.textContent = "NICE!"
-            score++
-        }    
-
-            else {
-                displayValue.textContent = "TRY AGAIN"
-                secondsLeft = secondsLeft -- penalty;
-            }
+            displayValue.textContent = "NICE!";
+            points += 1;
+            displayScore.textContent = points;
+            console.log(points);
+            i++;
+            
+        } else {
+            secondsLeft = secondsLeft - penalty;
+            displayValue.textContent = "TRY AGAIN" ; 
+        }
     }
-
+      
 }
-        // list items are buttons that trigger a correct / wrong output to rightWrong ID in HTML
-        
-        // store score in local memory
 
-        // progress to next question after right answer is selected
+
 
 // display page after timer is up or questions are over
 
 function gameOver() {
-    console.log("gameOver")
+    
+
+    // console.log("gameOver");
+   //window.location.replace("highscores.html");
+   
 };
